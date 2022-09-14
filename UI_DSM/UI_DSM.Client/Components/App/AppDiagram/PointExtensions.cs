846acb29ccb5b70e1e93028d6874b913efeb73bb
Martin Risseeuw <hello@martinr.nl>
2022-09-14 12:59:40 +0200

    feat: add links reconnection

---
+++ UI_DSM/UI_DSM.Client/Components/App/AppDiagram/PointExtensions.cs
@@ -0,0 +1,15 @@
+using Blazor.Diagrams.Core.Geometry;
+using System;
+
+namespace AppComponents
+{
+    public static class PointExtensions
+    {
+        public static double DistanceTo(this Point firstPoint, Point secondPoint)
+        {
+            var x = Math.Abs(firstPoint.X - secondPoint.X);
+            var y = Math.Abs(firstPoint.Y - secondPoint.Y);
+            return Math.Sqrt(x * x + y * y);
+        }
+    }
+}
