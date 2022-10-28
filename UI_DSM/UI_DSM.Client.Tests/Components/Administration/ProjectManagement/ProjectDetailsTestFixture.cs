eefcc9bd6363a3b2aff5cf7e975e8e3a47080927
nabilatrhea <112706228+nabilatrhea@users.noreply.github.com>
2022-10-28 16:59:10 +0200

    Participant management : Fix #104 (#134)
    * Project participants management
    
    * Project participants management
    
    * update deletion part
    
    * fix code smell
    
    * add ContentText to the confirm cancel popup

--- UI_DSM/UI_DSM.Client.Tests/Components/Administration/ProjectManagement/ProjectDetailsTestFixture.cs
+++ UI_DSM/UI_DSM.Client.Tests/Components/Administration/ProjectManagement/ProjectDetailsTestFixture.cs
@@ -2,7 +2,7 @@
 // <copyright file="ProjectDetailsTestFixture.cs" company="RHEA System S.A.">
 //  Copyright (c) 2022 RHEA System S.A.
 // 
-//  Author: Antoine Théate, Sam Gerené, Alex Vorobiev, Alexander van Delft
+//  Author: Antoine Théate, Sam Gerené, Alex Vorobiev, Alexander van Delft, Martin Risseeuw, Nabil Abbar
 // 
 //  This file is part of UI-DSM.
 //  The UI-DSM web application is used to review an ECSS-E-TM-10-25 model.
@@ -17,15 +17,19 @@ namespace UI_DSM.Client.Tests.Components.Administration.ProjectManagement
 
     using DevExpress.Blazor;
 
-    using Microsoft.AspNetCore.Components.Forms;
-
+    using Moq;
+    
     using NUnit.Framework;
 
     using UI_DSM.Client.Components.Administration.ProjectManagement;
+    using UI_DSM.Client.Services.Administration.ParticipantService;
+    using UI_DSM.Client.Services.Administration.RoleService;
     using UI_DSM.Client.Tests.Helpers;
     using UI_DSM.Client.ViewModels.Components.Administration.ProjectManagement;
+    using UI_DSM.Shared.DTO.Common;
+    using UI_DSM.Shared.Enumerator;
     using UI_DSM.Shared.Models;
-
+    using UI_DSM.Shared.Types;
     using TestContext = Bunit.TestContext;
 
     [TestFixture]
@@ -33,14 +37,18 @@ namespace UI_DSM.Client.Tests.Components.Administration.ProjectManagement
     {
         private TestContext context;
         private IProjectDetailsViewModel viewModel;
+        private Mock<IParticipantService> participantService;
+        private Mock<IRoleService> roleService;
 
         [SetUp]
         public void Setup()
         {
             this.context = new TestContext();
             this.context.ConfigureDevExpressBlazor();
+            this.participantService = new Mock<IParticipantService>();
+            this.roleService = new Mock<IRoleService>();
 
-            this.viewModel = new ProjectDetailsViewModel()
+            this.viewModel = new ProjectDetailsViewModel(participantService.Object, roleService.Object)
             {
                 Project = new Project(Guid.NewGuid())
                 {
@@ -65,6 +73,154 @@ namespace UI_DSM.Client.Tests.Components.Administration.ProjectManagement
 
             var inputText = renderer.FindComponent<DxTextBox>();
             Assert.That(inputText.Instance.Text, Is.EqualTo(this.viewModel.Project.ProjectName));
+        }
+
+        [Test]
+        public async Task VerifyOpenUpdateParticipant()
+        {
+            try
+            {
+
+                var roleId = Guid.NewGuid();
+                var newRole = new Role(roleId)
+                {
+                    RoleName = "Project Manager",
+                    AccessRights = new List<AccessRight>()
+                    {
+                        AccessRight.ManageParticipant
+                    }
+                };
+
+                var participant = new Participant()
+                {
+                    Role = new Role(Guid.NewGuid())
+                    {
+                        RoleName = "Project administrator",
+                        AccessRights = new List<AccessRight>()
+                        {
+                            AccessRight.ReviewTask
+                        }
+                    },
+                    User = new UserEntity(Guid.NewGuid())
+                    {
+                        UserName = "user"
+                    }
+                };
+
+                this.viewModel.Project.Participants.Add(participant);
+
+                var renderer = this.context.RenderComponent<ProjectDetails>(parameters =>
+                {
+                    parameters.Add(p => p.ViewModel, this.viewModel);
+                });
+                var participantName = renderer.Find("td");
+                Assert.That(participantName.InnerHtml, Does.Contain(participant.User.UserName));
+
+                var updateButton = renderer.FindComponent<DxButton>();
+                Assert.That(this.viewModel.IsOnUpdateViewMode, Is.False);
+                await renderer.InvokeAsync(updateButton.Instance.Click.InvokeAsync);
+
+                Assert.That(this.viewModel.IsOnUpdateViewMode, Is.True);
+
+                this.viewModel.ParticipantDetailsViewModel.Participant.Role = newRole;
+
+                this.participantService.Setup(x => x.UpdateParticipant(this.viewModel.ParticipantDetailsViewModel.Participant, this.viewModel.Project.Id))
+                    .ReturnsAsync(EntityRequestResponse<Participant>.Fail(new List<string>
+                    {
+                        "Failed to update user role"
+                    }));
+
+
+                await this.viewModel.ParticipantDetailsViewModel.OnValidSubmit.InvokeAsync();
+
+                Assert.Multiple(() =>
+                {
+                    Assert.That(this.viewModel.ErrorMessageViewModel.Errors, Has.Count.EqualTo(1));
+                    Assert.That(this.viewModel.IsOnUpdateViewMode, Is.True);
+                });
+
+                this.participantService.Setup(x => x.UpdateParticipant(this.viewModel.ParticipantDetailsViewModel.Participant, this.viewModel.Project.Id))
+                    .ReturnsAsync(EntityRequestResponse<Participant>.Success(new Participant()));
+
+                await this.viewModel.ParticipantDetailsViewModel.OnValidSubmit.InvokeAsync();
+
+                Assert.Multiple(() =>
+                {
+                    Assert.That(this.viewModel.IsOnUpdateViewMode, Is.False);
+                    Assert.That(this.viewModel.ErrorMessageViewModel.Errors, Is.Empty);
+                });
+
+                this.participantService.Setup(x => x.UpdateParticipant(this.viewModel.ParticipantDetailsViewModel.Participant, this.viewModel.Project.Id))
+                    .ThrowsAsync(new HttpRequestException());
+
+                await this.viewModel.ParticipantDetailsViewModel.OnValidSubmit.InvokeAsync();
+                Assert.That(this.viewModel.ErrorMessageViewModel.Errors, Has.Count.EqualTo(1));
+            }
+            catch
+            {
+                // On GitHub, exception is thrown even if the JSRuntime has been configured
+            }
+        }
+
+        [Test]
+        public async Task VerifyDeleteParticipant()
+        {
+
+            var participant1 = new Participant(Guid.NewGuid())
+            {
+                Role = new Role(Guid.NewGuid())
+                {
+                    RoleName = "Project administrator",
+                    AccessRights = new List<AccessRight>()
+                    {
+                        AccessRight.ReviewTask
+                    }
+                },
+                User = new UserEntity(Guid.NewGuid())
+                {
+                    UserName = "user1"
+                }
+            };
+
+            var participant2 = new Participant(Guid.NewGuid())
+            {
+                Role = new Role(Guid.NewGuid())
+                {
+                    RoleName = "Project administrator",
+                    AccessRights = new List<AccessRight>()
+                    {
+                        AccessRight.ReviewTask
+                    }
+                },
+                User = new UserEntity(Guid.NewGuid())
+                {
+                    UserName = "user2"
+                }
+            };
+
+            this.viewModel.Project.Participants.Add(participant1);
+            this.viewModel.Project.Participants.Add(participant2);
+
+            var renderer = this.context.RenderComponent<ProjectDetails>(parameters =>
+            {
+                parameters.Add(p => p.ViewModel, this.viewModel);
+            });
+
+            var deleteButton = renderer.FindComponents<DxButton>()
+                .FirstOrDefault(x => x.Instance.RenderStyle == ButtonRenderStyle.Danger);
+
+            Assert.That(this.viewModel.ConfirmCancelPopup.IsVisible, Is.False);
+            await renderer.InvokeAsync(() => deleteButton!.Instance.Click.InvokeAsync());
+
+            Assert.That(this.viewModel.ConfirmCancelPopup.IsVisible, Is.True);
+
+            this.participantService.Setup(x => x.DeleteParticipant(It.IsAny<Participant>(), this.viewModel.Project.Id)).ReturnsAsync(new RequestResponseDto()
+            {
+                IsRequestSuccessful = true
+            });
+
+            await renderer.InvokeAsync(() => this.viewModel.ConfirmCancelPopup.OnConfirm.InvokeAsync());
+            Assert.That(this.viewModel.Project.Participants, Has.Count.EqualTo(1));
         }
     }
 }
--- UI_DSM/UI_DSM.Client/ViewModels/Components/Administration/ProjectManagement/ProjectDetailsViewModel.cs
+++ UI_DSM/UI_DSM.Client/ViewModels/Components/Administration/ProjectManagement/ProjectDetailsViewModel.cs
@@ -2,7 +2,7 @@
 // <copyright file="ProjectDetailsViewModel.cs" company="RHEA System S.A.">
 //  Copyright (c) 2022 RHEA System S.A.
 // 
-//  Author: Antoine Théate, Sam Gerené, Alex Vorobiev, Alexander van Delft
+//  Author: Antoine Théate, Sam Gerené, Alex Vorobiev, Alexander van Delft, Martin Risseeuw, Nabil Abbar
 // 
 //  This file is part of UI-DSM.
 //  The UI-DSM web application is used to review an ECSS-E-TM-10-25 model.
@@ -13,9 +13,15 @@
 
 namespace UI_DSM.Client.ViewModels.Components.Administration.ProjectManagement
 {
+    using DevExpress.Blazor;
+    using DynamicData;
+    using Microsoft.AspNetCore.Components;
     using ReactiveUI;
 
     using UI_DSM.Client.Components.Administration.ProjectManagement;
+    using UI_DSM.Client.Services.Administration.ParticipantService;
+    using UI_DSM.Client.Services.Administration.RoleService;
+    using UI_DSM.Client.ViewModels.Components.Administration.ParticipantManagement;
     using UI_DSM.Shared.Models;
 
     /// <summary>
@@ -35,6 +41,151 @@ namespace UI_DSM.Client.ViewModels.Components.Administration.ProjectManagement
         {
             get => this.project;
             set => this.RaiseAndSetIfChanged(ref this.project, value);
+        }
+
+        /// <summary>
+        ///     The <see cref="IParticipantService" />
+        /// </summary>
+        private readonly IParticipantService participantService;
+
+        /// <summary>
+        ///     The <see cref="IRoleService" />
+        /// </summary>
+        private readonly IRoleService roleService;
+
+        /// <summary>
+        ///     The <see cref="IParticipantDetailsViewModel" />
+        /// </summary>
+        public IParticipantDetailsViewModel ParticipantDetailsViewModel { get; private set; } = new ParticipantDetailsViewModel();
+
+        /// <summary>
+        ///     Backing field for <see cref="IsOnUpdateViewMode" />
+        /// </summary>
+        private bool isOnUpdateViewMode;
+
+        /// <summary>
+        ///     The user that the administrator is going to delete
+        /// </summary>
+        private Participant participantToDelete;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ProjectDetailsViewModel" /> class.
+        /// </summary>
+        public ProjectDetailsViewModel(IParticipantService participantService, IRoleService roleService)
+        {
+            this.participantService = participantService;
+            this.roleService = roleService;
+
+            this.ParticipantDetailsViewModel = new ParticipantDetailsViewModel
+            {
+                OnValidSubmit = new EventCallbackFactory().Create(this, this.UpdateParticipant)
+            };
+
+            this.ConfirmCancelPopup = new ConfirmCancelPopupViewModel
+            {
+                ConfirmRenderStyle = ButtonRenderStyle.Danger,
+                OnCancel = new EventCallbackFactory().Create(this, this.OnCancelDelete),
+                OnConfirm = new EventCallbackFactory().Create(this, this.OnConfirmDelete),
+                ContentText = "Are you sure to want to remove this participant ?",
+                HeaderText = "Are you sure ?"
+            };
+        }
+
+        /// <summary>
+        ///     The <see cref="IConfirmCancelPopupViewModel" />
+        /// </summary>
+        public IConfirmCancelPopupViewModel ConfirmCancelPopup { get; private set; }
+
+        /// <summary>
+        ///     Opens a popup to confirm to delete a participant
+        /// </summary>
+        /// <param name="participant">The <see cref="Participant" /> to remove</param>
+        public void AskConfirmDeleteUser(Participant participant)
+        {
+            this.participantToDelete = participant;
+            this.ConfirmCancelPopup.ContentText = $"You are about to delete the participant {this.participantToDelete.User.UserName}.\nAre you sure?";
+            this.ConfirmCancelPopup.IsVisible = true;
+        }
+
+        /// <summary>
+        ///     Opens a popup to provide informations for the given <see cref="Participant" />
+        /// </summary>
+        /// <param name="participant">The <see cref="Participant" /></param>
+        public async Task OpenUpdatePopup(Participant participant)
+        {
+            this.ParticipantDetailsViewModel.Participant = participant;
+            this.ParticipantDetailsViewModel.AvailableRoles = await roleService.GetRoles();
+            this.IsOnUpdateViewMode = true;
+        }
+
+        /// <summary>
+        ///     Value indicating the administrator is currently updating roles of a  <see cref="Participant"/>
+        /// </summary>
+        public bool IsOnUpdateViewMode
+        {
+            get => this.isOnUpdateViewMode;
+            set => this.RaiseAndSetIfChanged(ref this.isOnUpdateViewMode, value);
+        }
+
+        /// <summary>
+        ///     Callback used when the confirmation dialog has confirmed the deletion
+        /// </summary>
+        /// <returns>A <see cref="Task" /></returns>
+        private async Task OnConfirmDelete()
+        {
+            var deleteResult = await this.participantService.DeleteParticipant(this.participantToDelete, this.project.Id);
+
+            if (deleteResult.IsRequestSuccessful)
+            {
+                this.Project.Participants.Remove(this.participantToDelete);
+            }
+
+            this.OnCancelDelete();
+        }
+
+        /// <summary>
+        ///     Callback used when the confirmation dialog has canceled the deletion
+        /// </summary>
+        private void OnCancelDelete()
+        {
+            this.ConfirmCancelPopup.IsVisible = false;
+            this.participantToDelete = null;
+        }
+
+        /// <summary>
+        ///     The <see cref="IErrorMessageViewModel" />
+        /// </summary>
+        public IErrorMessageViewModel ErrorMessageViewModel { get; } = new ErrorMessageViewModel();
+
+        /// <summary>
+        ///     Tries to update a <see cref="Participant" />
+        /// </summary>
+        /// <returns>A <see cref="Task" /></returns>
+        private async Task UpdateParticipant()
+        {
+            try
+            {
+                var updateResult = await this.participantService.UpdateParticipant(this.ParticipantDetailsViewModel.Participant, this.project.Id);
+                this.ErrorMessageViewModel.Errors.Clear();
+
+                if (updateResult.Errors.Any())
+                {
+                    this.ErrorMessageViewModel.Errors.AddRange(updateResult.Errors);
+                }
+
+                if (updateResult.IsRequestSuccessful)
+                {
+                    this.ParticipantDetailsViewModel.Participant = updateResult.Entity;
+                }
+                
+                this.IsOnUpdateViewMode = !updateResult.IsRequestSuccessful;
+            
+            }
+            catch (Exception exception)
+            {
+                this.ErrorMessageViewModel.Errors.Clear();
+                this.ErrorMessageViewModel.Errors.Add(exception.Message);
+            }
         }
     }
 }
