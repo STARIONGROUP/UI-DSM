a2378c6073bf328c01babeb3c9005759201a7559
Nabil Abbar <n.abbar@rheagroup.com>
2022-11-18 02:11:39 +0100

    handle js runtime in bunit test

--- UI_DSM/UI_DSM.Client.Tests/Components/NormalUser/ProjectReview/ReviewObjectiveCreationTestFixture.cs
+++ UI_DSM/UI_DSM.Client.Tests/Components/NormalUser/ProjectReview/ReviewObjectiveCreationTestFixture.cs
@@ -68,7 +68,7 @@ namespace UI_DSM.Client.Tests.Components.NormalUser.ProjectReview
         }
 
         [Test]
-        public void VerifyComponent()
+        public async Task VerifyComponent()
         {
             try
             {
