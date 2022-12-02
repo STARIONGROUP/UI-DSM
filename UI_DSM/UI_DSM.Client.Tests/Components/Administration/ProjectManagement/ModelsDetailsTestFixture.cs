12155c47495bf50eac4f209d0042e1d6e26dfecb
jaimeatrhea <j.bernar@rheagroup.com>
2022-12-02 14:55:30 +0100

    styles

--- UI_DSM/UI_DSM.Client.Tests/Components/Administration/ProjectManagement/ModelsDetailsTestFixture.cs
+++ UI_DSM/UI_DSM.Client.Tests/Components/Administration/ProjectManagement/ModelsDetailsTestFixture.cs
@@ -1,24 +1,36 @@
-﻿using Bunit;
-using DevExpress.Blazor;
-using Moq;
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UI_DSM.Client.Components.Administration.ProjectManagement;
-using UI_DSM.Client.Services.Administration.ParticipantService;
-using UI_DSM.Client.Services.Administration.RoleService;
-using UI_DSM.Client.Tests.Helpers;
-using UI_DSM.Client.ViewModels.Components.Administration.ProjectManagement;
-using UI_DSM.Shared.Enumerator;
-using UI_DSM.Shared.Models;
-using UI_DSM.Shared.Types;
-using TestContext = Bunit.TestContext;
+﻿// --------------------------------------------------------------------------------------------------------
+// <copyright file="ModelsDetailsTestFixture.cs" company="RHEA System S.A.">
+//  Copyright (c) 2022 RHEA System S.A.
+// 
+//  Author: Antoine Théate, Sam Gerené, Alex Vorobiev, Alexander van Delft, Martin Risseeuw, Nabil Abbar, Jaime Bernar
+// 
+//  This file is part of UI-DSM.
+//  The UI-DSM web application is used to review an ECSS-E-TM-10-25 model.
+// 
+//  The UI-DSM application is provided to the community under the Apache License 2.0.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------
+
 
 namespace UI_DSM.Client.Tests.Components.Administration.ProjectManagement
 {
+    using System;
+    using System.Collections.Generic;
+
+    using Bunit;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    using UI_DSM.Client.Components.Administration.ProjectManagement;
+    using UI_DSM.Client.Services.Administration.ParticipantService;
+    using UI_DSM.Client.Services.Administration.RoleService;
+    using UI_DSM.Client.Tests.Helpers;
+    using UI_DSM.Client.ViewModels.Components.Administration.ProjectManagement;
+    using UI_DSM.Shared.Enumerator;
+    using UI_DSM.Shared.Models;
+
     using TestContext = Bunit.TestContext;
 
     [TestFixture]
